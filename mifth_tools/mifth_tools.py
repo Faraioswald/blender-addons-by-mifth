2587d89ce8fcc86247a56aaccd6b46b03ab22b24
mifth <mifth@localhost>
2014-09-07 15:32:51 +0400

    world matrix

--- mifth_tools/mifth_tools.py
+++ mifth_tools/mifth_tools.py
@@ -81,6 +81,7 @@ class MFTPanelAnimation(bpy.types.Panel):
         layout.separator()
         layout.operator("mft.morfcreator", text="Morf Creator")
         layout.prop(mifthTools, "morfCreatorNames")
+        layout.prop(mifthTools, "morfUseWorldMatrix", text='useWorldMatrix')
 
 
 class MFTPanelPlaykot(bpy.types.Panel):
@@ -376,7 +377,10 @@ class MFTMorfCreator(bpy.types.Operator):
                             shapeKey.name = obj.name
 
                         for vert in obj.data.vertices:
-                            shapeKey.data[vert.index].co = vert.co
+                            if mifthTools.morfUseWorldMatrix:
+                                shapeKey.data[vert.index].co = obj.matrix_world * vert.co
+                            else:
+                                shapeKey.data[vert.index].co = vert.co
                             #print(vert.co)  # this is a vertex coord of the mesh
                     else:
                         self.report({'INFO'}, "Model " + obj.name + " has different points count")
